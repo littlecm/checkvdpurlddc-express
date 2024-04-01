3b86d362d4f25ed17ee8cfe41dbd54010c2a6e74
littlecm <clittle.norml@gmail.com>
2024-04-01 15:42:36 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the VDP Checker</h1>
+      <p>Use the /api/check endpoint to check for redirects.</p>
+    </div>
+  )
+}
